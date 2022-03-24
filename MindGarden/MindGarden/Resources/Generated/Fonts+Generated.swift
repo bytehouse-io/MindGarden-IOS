8d39e8d7dd83aeca52f8fd0c394f144a7e09b5a9
Vishal0119 <84787283+Vishal0119@users.noreply.github.com>
2022-03-24 19:48:03 +0530

    fix : madastyle issue

--- MindGarden/MindGarden/Resources/Generated/Fonts+Generated.swift
+++ MindGarden/MindGarden/Resources/Generated/Fonts+Generated.swift
@@ -6,7 +6,7 @@ extension Font {
 	/// Create a custom font with the given name and size that scales relative to the given textStyle.
 	///
 	/// Font is static in iOS 13.0
-	public static func madaDynamic(_ style: MadaStyle, size: CGFloat, relativeTo textStyle: TextStyle = .body) -> Font {
+	static func madaDynamic(_ style: MadaStyle, size: CGFloat, relativeTo textStyle: TextStyle = .body) -> Font {
 		if #available(iOS 14.0, *) {
 			return Font.custom(style.rawValue, size: size, relativeTo: textStyle)
 		} else {
@@ -15,23 +15,23 @@ extension Font {
 	}
 
 	/// Create a custom font with the given name and size that scales with the body text style.
-	public static func mada(_ style: MadaStyle, size: CGFloat) -> Font {
+	static func mada(_ style: MadaStyle, size: CGFloat) -> Font {
 		return Font.custom(style.rawValue, size: size)
 	}
 
 	/// Create a custom font with the given name and a fixed size that does not scale with Dynamic Type.
 	@available(iOS 14.0, *)
-	public static func mada(_ style: MadaStyle, fixedSize: CGFloat) -> Font {
+	static func mada(_ style: MadaStyle, fixedSize: CGFloat) -> Font {
 		return Font.custom(style.rawValue, fixedSize: fixedSize)
 	}
 
 	/// Create a custom font with the given name and size that scales relative to the given textStyle.
 	@available(iOS 14.0, *)
-	public static func mada(_ style: MadaStyle, size: CGFloat, relativeTo textStyle: TextStyle = .body) -> Font {
+	static func mada(_ style: MadaStyle, size: CGFloat, relativeTo textStyle: TextStyle = .body) -> Font {
 		return Font.custom(style.rawValue, size: size, relativeTo: textStyle)
 	}
 
-	public enum MadaStyle: String {
+	enum MadaStyle: String {
 		case bold = "Mada-Bold"
 		case light = "Mada-Light"
 		case medium = "Mada-Medium"
