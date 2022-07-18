e0bb475e8b2d671e6c582604ea3ab6d90bc12946
Vishal Davara <>
2022-07-18 18:24:32 +0530

    Fix : Tab bar bug

--- MindGarden/MindGarden/Resources/Generated/Images+Generated.swift
+++ MindGarden/MindGarden/Resources/Generated/Images+Generated.swift
@@ -4,6 +4,7 @@ import SwiftUI
 
 /// Cases to reference for safely initializing images, like so: `Img.Onboarding.header`
 public enum Img {
+  public static let house = Image("House")
   public static let iceFlower = Image("Ice Flower")
   public static let bad = Image("bad")
   public static let good = Image("good")
@@ -193,7 +194,6 @@ public enum Img {
   public static let hands = Image("hands")
   public static let happySunflower = Image("happySunflower")
   public static let heart = Image("heart")
-  public static let house = Image("house")
   public static let icecream = Image("icecream")
   public static let alpha = Image("alpha")
   public static let beach = Image("beach")
