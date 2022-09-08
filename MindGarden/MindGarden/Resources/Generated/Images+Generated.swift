1943785aae3430d95db9ba313e6c7691807c2aa2
Vishal Davara <>
2022-09-08 23:45:07 +0530

    Fix : conflict in journal image

--- MindGarden/MindGarden/Resources/Generated/Images+Generated.swift
+++ MindGarden/MindGarden/Resources/Generated/Images+Generated.swift
@@ -4,6 +4,7 @@ import SwiftUI
 
 /// Cases to reference for safely initializing images, like so: `Img.Onboarding.header`
 public enum Img {
+  public static let house = Image("House")
   public static let iceFlower = Image("Ice Flower")
   public static let bad = Image("bad")
   public static let good = Image("good")
@@ -213,7 +214,6 @@ public enum Img {
   public static let happyPandaFace = Image("happyPandaFace")
   public static let happySunflower = Image("happySunflower")
   public static let heart = Image("heart")
-  public static let house = Image("house")
   public static let icecream = Image("icecream")
   public static let alpha = Image("alpha")
   public static let beach = Image("beach")
@@ -238,6 +238,7 @@ public enum Img {
   public static let six = Image("six")
   public static let theta = Image("theta")
   public static let investMg = Image("investMg")
+  public static let journelBG = Image("journelBG")
   public static let journeyBackground = Image("journeyBackground")
   public static let juiceBoxes = Image("juiceBoxes")
   public static let kidStudying = Image("kidStudying")
