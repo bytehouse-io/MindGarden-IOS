ed93e2175814689d5d674c5227bc9db7aac2d7e8
Vishal Davara <vishal.davara@ovyo.com>
2022-09-26 21:22:17 +0530

    Feature : added emoji bubble up animation on streakscene.

--- MindGarden/MindGarden/Views/StreakView/StreakScene.swift
+++ MindGarden/MindGarden/Views/StreakView/StreakScene.swift
@@ -27,6 +27,8 @@ struct StreakScene: View {
     @State private var showNextSteps = false
     @State private var showBackground = false
     
+    @State var replay: Bool = false
+    
     var subTitle : String {
         switch bonusModel.streakNumber {
         case 1:  return "👣 A journey of a thousand miles begins with a single step - Lao Tzu"
@@ -121,6 +123,9 @@ struct StreakScene: View {
                 }
             }
             .offset(y: -145)
+            if replay {
+                BubbleEffectView(replay: $replay)
+            }
         }
 
         .onChange(of: isSharePresented) { value in
@@ -131,6 +136,9 @@ struct StreakScene: View {
         }
         .onAppearAnalytics(event: .screen_load_streak)
         .onAppear {
+            if [3,7,10,14,21, 30].contains(bonusModel.streakNumber) {
+                replay = true
+            }
             Amplitude.instance().logEvent("streak_logged", withEventProperties: ["number": bonusModel.streakNumber])
             MGAudio.sharedInstance.stopSound()
             MGAudio.sharedInstance.playSounds(soundFileNames: ["fire_ignite.mp3","fire.mp3"])
@@ -213,4 +221,122 @@ extension View {
         window.makeKeyAndVisible()
         return hosting.view.renderedImage
     }
+}
+
+
+struct BubbleEffectView: View {
+    @StateObject var viewModel: BubbleEffectViewModel = BubbleEffectViewModel()
+    @Binding var replay: Bool
+    var body: some View {
+        GeometryReader{ geo in
+            ZStack{
+                //Show bubble views for each bubble
+                ForEach(viewModel.bubbles){bubble in
+                    BubbleView(bubble: bubble)
+                }
+            }.onChange(of: replay, perform: { _ in
+                viewModel.addBubbles(frameSize: geo.size)
+            })
+            
+            .onAppear(){
+                //Set the initial position from frame size
+                viewModel.viewBottom = geo.size.height
+                viewModel.addBubbles(frameSize: geo.size)
+            }
+        }
+    }
+}
+class BubbleEffectViewModel: ObservableObject{
+    @Published var viewBottom: CGFloat = CGFloat.zero
+    @Published var bubbles: [BubbleViewModel] = []
+    private var timer: Timer?
+    private var timerCount: Int = 0
+    @Published var bubbleCount: Int = 50
+    
+    func addBubbles(frameSize: CGSize){
+        let lifetime: TimeInterval = 2
+        //Start timer
+        timerCount = 0
+        if timer != nil{
+            timer?.invalidate()
+        }
+        timer = Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true) { (timer) in
+            let bubble = BubbleViewModel(height: 50, width: 50, x: frameSize.width/2, y: self.viewBottom, color: .white, lifetime: lifetime)
+            //Add to array
+            self.bubbles.append(bubble)
+            //Get rid if the bubble at the end of its lifetime
+            Timer.scheduledTimer(withTimeInterval: bubble.lifetime, repeats: false, block: {_ in
+                self.bubbles.removeAll(where: {
+                    $0.id == bubble.id
+                })
+            })
+            if self.timerCount >= self.bubbleCount {
+                //Stop when the bubbles will get cut off by screen
+                timer.invalidate()
+                self.timer = nil
+            }else{
+                self.timerCount += 1
+            }
+        }
+    }
+}
+struct BubbleView: View {
+    //If you want to change the bubble's variables you need to observe it
+    @ObservedObject var bubble: BubbleViewModel
+    @State var opacity: Double = 0
+    var body: some View {
+        Text("🔥")
+            .frame(width: 50, height: 50)
+            .position(x: CGFloat.random(in:40...UIScreen.screenWidth), y: bubble.y)
+            .onAppear {
+                
+                withAnimation(.linear(duration: bubble.lifetime)){
+                    //Go up
+                    self.bubble.y = -bubble.height
+                    //Go sideways
+                    self.bubble.x += bubble.xFinalValue()
+                    //Change size
+                    let width = bubble.yFinalValue()
+                    self.bubble.width = width
+                    self.bubble.height = width
+                }
+                //Change the opacity faded to full to faded
+                //It is separate because it is half the duration
+                DispatchQueue.main.asyncAfter(deadline: .now()) {
+                    withAnimation(.linear(duration: bubble.lifetime/2).repeatForever()) {
+                        self.opacity = 1
+                    }
+                }
+                DispatchQueue.main.asyncAfter(deadline: .now()) {
+                    withAnimation(Animation.linear(duration: bubble.lifetime/4).repeatForever()) {
+                        //Go sideways
+                        //bubble.x += bubble.xFinalValue()
+                    }
+                }
+            }
+    }
+}
+class BubbleViewModel: Identifiable, ObservableObject{
+    let id: UUID = UUID()
+    @Published var x: CGFloat
+    @Published var y: CGFloat
+    @Published var color: Color
+    @Published var width: CGFloat
+    @Published var height: CGFloat
+    @Published var lifetime: TimeInterval = 0
+    init(height: CGFloat, width: CGFloat, x: CGFloat, y: CGFloat, color: Color, lifetime: TimeInterval){
+        self.height = height
+        self.width = width
+        self.color = color
+        self.x = x
+        self.y = y
+        self.lifetime = lifetime
+    }
+    func xFinalValue() -> CGFloat {
+        return CGFloat.random(in:-width*CGFloat(lifetime*2.5)...width*CGFloat(lifetime*2.5))
+    }
+    func yFinalValue() -> CGFloat {
+        return CGFloat.random(in:0...width*CGFloat(lifetime*2.5))
+    }
+    
 }
